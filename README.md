062f0edf90e885dfe39c921265b4ed777ce3379b
bearbb <nguyen.b2uty@gmail.com>
2021-12-13 17:15:00 +0700

    Add add, remove, update on temp and rain data

--- README.md
+++ README.md
@@ -9,3 +9,71 @@ GET /api/tempNRainDataByNameAndYear/:PVName/:year
 GET /api/tempNRainPrecise/:PVName/:year/:month
 Example: get temp and rain amount of BaRia in 2014 on JAN
 GET /api/tempNRainPrecise/BaRia/2014/JAN
+
+## Those api below need to be moderator role
+
+## Add temp data for a year
+
+POST /api/addTempData/:PVName
+
+### Data structure:
+
+{
+"YEAR": number,
+"JAN": number,
+"FEB": number,
+"MAR": number,
+"APR": number,
+"MAY": number,
+"JUN": number,
+"JUL": number,
+"AUG": number,
+"SEP": number,
+"OCT": number,
+"NOV": number,
+"DEC": number,
+"ANN": number
+}
+
+## Add rain amount data for a year
+
+POST /api/addRainData/:PVName
+
+### Data structure:
+
+{
+"YEAR": number,
+"JAN": number,
+"FEB": number,
+"MAR": number,
+"APR": number,
+"MAY": number,
+"JUN": number,
+"JUL": number,
+"AUG": number,
+"SEP": number,
+"OCT": number,
+"NOV": number,
+"DEC": number,
+"ANN": number
+}
+
+## Update temp or rain amount data for a year
+
+POST /api/updateTempData/:PVName
+
+POST /api/updateRainData/:PVName
+
+### Data structure
+
+Similar to add but optional in amount of data u want to update. Example: update only JAN temp:
+{
+"YEAR": 2020,
+"JAN": 30
+}
+
+## Delete temp or rain amount data for a year
+
+DELETE /api/deleteTempData/:PVName/:year
+
+DELETE /api/deleteRainData/:PVName/:year
--- routes/temperature.routes.js
+++ routes/temperature.routes.js
@@ -45,4 +45,184 @@ module.exports = function (app) {
       }
     }
   );
+
+  //add new data for temp
+  app.post(
+    "/api/addTempData/:PVName",
+    [authJwt.verifyToken, authJwt.isModerator],
+    async (req, res, next) => {
+      try {
+        let data = req.body;
+        console.log(data.YEAR);
+        //check if data exist for that year
+        let queryData = await db[`${req.params.PVName}`].find({
+          YEAR: data.YEAR,
+          PARAMETER: "T2M",
+        });
+        //not exist then create new one
+        console.log(queryData);
+        if (queryData[0] == undefined) {
+          const newData = new db[req.params.PVName]({
+            ...data,
+            PARAMETER: "T2M",
+          });
+          const saveData = await newData.save();
+          console.log(saveData);
+          res.status(200).json({ msg: "Data add successfully" });
+        } else {
+          console.log("Data already existed");
+          res.status(400).json({ msg: "Data already added" });
+        }
+      } catch (error) {
+        res.status(500).json({ msg: "Something went wrong" });
+      }
+    }
+  );
+  //add new data for rain amount
+  app.post(
+    "/api/addRainData/:PVName",
+    [authJwt.verifyToken, authJwt.isModerator],
+    async (req, res, next) => {
+      try {
+        let data = req.body;
+        console.log(data.YEAR);
+        //check if data exist for that year
+        let queryData = await db[`${req.params.PVName}`].find({
+          YEAR: data.YEAR,
+          PARAMETER: "RH2M",
+        });
+        //not exist then create new one
+        console.log(queryData);
+        if (queryData[0] == undefined) {
+          const newData = new db[req.params.PVName]({
+            ...data,
+            PARAMETER: "RH2M",
+          });
+          const saveData = await newData.save();
+          console.log(saveData);
+          res.status(200).json({ msg: "Data add successfully" });
+        } else {
+          console.log("Data already existed");
+          res.status(400).json({ msg: "Data already added" });
+        }
+      } catch (error) {
+        res.status(500).json({ msg: "Something went wrong" });
+      }
+    }
+  );
+
+  //Delete data of temp in a year
+  app.delete(
+    "/api/deleteTempData/:PVName/:year",
+    [authJwt.verifyToken, authJwt.isModerator],
+    async (req, res, next) => {
+      try {
+        const queryData = await db[`${req.params.PVName}`].find({
+          YEAR: req.params.year,
+          PARAMETER: "T2M",
+        });
+        //check if doc exist
+        if (queryData[0] != undefined) {
+          const deletingData = await db[`${req.params.PVName}`].deleteOne({
+            YEAR: req.params.year,
+            PARAMETER: "T2M",
+          });
+          console.log(deletingData);
+          res.status(200).json({ msg: "delete successfully" });
+        } else {
+          res.status(400).json({ msg: "data not exist" });
+        }
+      } catch (error) {
+        res.status(500).json({ msg: "something went wrong" });
+      }
+    }
+  );
+  //Delete data of rain amount in a year
+  app.delete(
+    "/api/deleteRainData/:PVName/:year",
+    [authJwt.verifyToken, authJwt.isModerator],
+    async (req, res, next) => {
+      try {
+        const queryData = await db[`${req.params.PVName}`].find({
+          YEAR: req.params.year,
+          PARAMETER: "RH2M",
+        });
+        //check if doc exist
+        if (queryData[0] != undefined) {
+          const deletingData = await db[`${req.params.PVName}`].deleteOne({
+            YEAR: req.params.year,
+            PARAMETER: "RH2M",
+          });
+          console.log(deletingData);
+          res.status(200).json({ msg: "delete successfully" });
+        } else {
+          res.status(400).json({ msg: "data not exist" });
+        }
+      } catch (error) {
+        res.status(500).json({ msg: "something went wrong" });
+      }
+    }
+  );
+
+  //update rain data in a year
+  app.post(
+    "/api/updateRainData/:PVName",
+    [authJwt.verifyToken, authJwt.isModerator],
+    async (req, res, next) => {
+      let updateData = req.body;
+      console.log(updateData);
+      try {
+        //query 1st to check if exist
+        const queryData = await db[`${req.params.PVName}`].find({
+          YEAR: updateData.YEAR,
+          PARAMETER: "RH2M",
+        });
+        //data exist
+        if (queryData[0] != undefined) {
+          const updatingData = await db[`${req.params.PVName}`].updateOne(
+            {
+              YEAR: updateData.YEAR,
+              PARAMETER: "RH2M",
+            },
+            updateData
+          );
+          console.log(updatingData);
+          res.status(200).json({ msg: "update successfully" });
+        } else {
+          res.status(400).json({ msg: "data not exist" });
+        }
+      } catch (error) {}
+    }
+  );
+
+  //update temp data in a year
+  app.post(
+    "/api/updateTempData/:PVName",
+    [authJwt.verifyToken, authJwt.isModerator],
+    async (req, res, next) => {
+      let updateData = req.body;
+      console.log(updateData);
+      try {
+        //query 1st to check if exist
+        const queryData = await db[`${req.params.PVName}`].find({
+          YEAR: updateData.YEAR,
+          PARAMETER: "T2M",
+        });
+        //data exist
+        if (queryData[0] != undefined) {
+          const updatingData = await db[`${req.params.PVName}`].updateOne(
+            {
+              YEAR: updateData.YEAR,
+              PARAMETER: "T2M",
+            },
+            updateData
+          );
+          console.log(updatingData);
+          res.status(200).json({ msg: "update successfully" });
+        } else {
+          res.status(400).json({ msg: "data not exist" });
+        }
+      } catch (error) {}
+    }
+  );
 };
