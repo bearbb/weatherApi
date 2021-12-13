ca65af3603920c0f0b8343f626f81e405cabdc94
bearbb <nguyen.b2uty@gmail.com>
2021-12-13 14:53:23 +0700

    Add auth stuff

---
+++ config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "bundau-mamtom-nhieuquat",
+};
---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  URI: "mongodb+srv://realcua:bunrealcuakhongmamtom@cluster0.8vnpv.mongodb.net/weatherDB",
+};
